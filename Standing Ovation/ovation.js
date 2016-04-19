68ae3815cd4df34de467ad70c09555a9a2926347
Sharun Kumar <sharunks@ymail.com>
2016-04-19 16:03:40 +0530

    Code Tidy

--- Standing Ovation/ovation.js
+++ Standing Ovation/ovation.js
@@ -1,23 +1,22 @@
 var fs = require('fs');
 
-var input = fs.readFileSync(process.argv[2],"utf-8");
+var input = fs.readFileSync(process.argv[2], "utf-8");
 
 input = input.split('\n');
 input.shift();
 input.pop();
-for(var i=0; i<input.length; i++) {
+for (var i = 0; i < input.length; i++) {
 	var x = input[i].split(' ').pop();
-	//console.log(x);
-	console.log("Case #"+(i+1)+":",check(toIntArray(x)));
+	console.log("Case #" + (i + 1) + ":", check(toIntArray(x)));
 }
 
 function toIntArray(x) {
 	var y = [];
-	for(var i=0; i<x.length; i++) y.push(parseInt(x[i]));
+	for (var i = 0; i < x.length; i++) y.push(parseInt(x[i]));
 	return y;
 }
 
-function max(x,y) {
+function max(x, y) {
 	return x > y ? x : y;
 }
 
@@ -25,12 +24,12 @@ function check(x) {
 	var guests = 0;
 	for (var i = x.length - 1; i >= 0; i--) {
 		var sum = 0;
-		if (x[i] > 0) {			
+		if (x[i] > 0) {
 			for (j = i - 1; j >= 0; j--) {
 				sum += x[j];
 			}
-			if(sum < i) {
-				guests = max(guests,i-sum)
+			if (sum < i) {
+				guests = max(guests, i - sum)
 			}
 		}
 	}
